89553dd08c8e8211da0be5c7909636354317cbff
Pavel Rabekin <pavelrabekin@gmail.com>
2022-10-03 10:01:05 +0300

    added index.html

--- users/views.py
+++ users/views.py
@@ -6,39 +6,42 @@ from users.forms import RegisterForm, LoginForm
 from users.models import User
 
 
-def register_user(request):
-    if request.user.is_authenticated:
-        return redirect("index")
+def index(request):
+    return render(request, "index.html")
 
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = User(email=form.cleaned_data["email"])
-            user.set_password(form.cleaned_data["password"])
-            user.save()
-            return redirect("login")
-    else:
-        form = RegisterForm()
-        return render(request, "register.html", {"form": form})
-
-
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect("index")
-
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(request=request, **form.cleaned_data)
-            if user is None:
-                return HttpResponse("BadRequest", status=400)
-            login(request, user)
-            return redirect("index")
-    else:
-        form = LoginForm()
-        return render(request, "login_view.html", {"form": form})
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("index")
+# def register_user(request):
+#     if request.user.is_authenticated:
+#         return redirect("index")
+#
+#     if request.method == "POST":
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             user = User(email=form.cleaned_data["email"])
+#             user.set_password(form.cleaned_data["password"])
+#             user.save()
+#             return redirect("login")
+#     else:
+#         form = RegisterForm()
+#         return render(request, "register.html", {"form": form})
+#
+#
+# def login_user(request):
+#     if request.user.is_authenticated:
+#         return redirect("index")
+#
+#     if request.method == "POST":
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             user = authenticate(request=request, **form.cleaned_data)
+#             if user is None:
+#                 return HttpResponse("BadRequest", status=400)
+#             login(request, user)
+#             return redirect("index")
+#     else:
+#         form = LoginForm()
+#         return render(request, "login_view.html", {"form": form})
+#
+#
+# def logout_user(request):
+#     logout(request)
+#     return redirect("index")
