7a2ae81d6bb0af7f5dca97526e376c8b9393804d
Pavel Rabekin <pavelrabekin@gmail.com>
2022-10-24 11:20:05 +0300

    update profile models

--- users/admin.py
+++ users/admin.py
@@ -11,7 +11,7 @@ class ProfileInline(admin.TabularInline):
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "is_staff", "is_superuser", "created_at")
-    fields = ("username", "is_staff", "is_superuser")
+    fields = ("username", "is_staff", "is_superuser", ("groups", "user_permissions",))
     list_filter = ("is_staff", "is_superuser")
     readonly_fields = ("created_at",)
     search_fields = ("username", "is_staff", "is_superuser")
