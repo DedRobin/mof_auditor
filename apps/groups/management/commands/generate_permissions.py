f31298d24d8a58e228de49abb99f800205d5b953
Pavel Rabekin <pavelrabekin@gmail.com>
2022-11-14 12:02:56 +0300

    updated management command for creating of default permissions

--- apps/groups/management/commands/generate_permissions.py
+++ apps/groups/management/commands/generate_permissions.py
@@ -7,11 +7,13 @@ class Command(BaseCommand):
     help = "Generate permissions"
 
     def handle(self, *args, **options):
+        Permission.objects.all().delete()
+
         default_permissions = (
-            ("All users | All groups | Can create the data", "all_all_create"),
-            ("All users | All groups | Can read the data", "all_all_read"),
-            ("All users | All groups | Can update the data", "all_all_update"),
-            ("All users | All groups | Can delete the data", "all_all_delete"),
+            ("All users | Can create the data", "all_create"),
+            ("All users | Can read the data", "all_read"),
+            ("All users | Can update the data", "all_update"),
+            ("All users | Can delete the data", "all_delete"),
         )
         for name, codename in default_permissions:
             Permission.objects.create(name=name, codename=codename)
