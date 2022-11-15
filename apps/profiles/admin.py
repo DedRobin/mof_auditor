0ab5a8b3fd192fe15c02a9125fec4d9f1e68598a
Pavel Rabekin <pavelrabekin@gmail.com>
2022-11-15 11:01:06 +0300

    update admin for 'Profile' model

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -9,12 +9,13 @@ class ProfileInline(admin.TabularInline):
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "gender", "first_name", "last_name", "created_at")
-    fields = ("user", "gender", "first_name", "last_name")
+    list_display = ("user", "email", "gender", "first_name", "last_name", "created_at")
+    fields = ("user", "email", "gender", "first_name", "last_name")
     list_filter = ("gender",)
     readonly_fields = ("created_at",)
     search_fields = (
         "user__username",
+        "email",
         "first_name",
         "last_name",
         "gender",
