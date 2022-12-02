b0890cb7a8282cb56450e89d35dae404b3e8c97c
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-02 16:13:27 +0300

    update admin for user

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -8,12 +8,8 @@ from import_export.admin import ImportExportModelAdmin
 
 @admin.register(User)
 class UserAdmin(ImportExportModelAdmin):
-    list_display = ("username", "is_staff", "is_superuser", "created_at")
-    fields = (
-        "username",
-        "is_staff",
-        "is_superuser",
-    )
+    list_display = ("username", "password", "is_staff", "is_superuser", "created_at")
+    fields = ("username", "password", "is_staff", "is_superuser",)
     list_filter = ("is_staff", "is_superuser")
     readonly_fields = ("created_at",)
     search_fields = ("username", "is_staff", "is_superuser")
