b0f1ee3a13ca0a5dccede4e66f0614d663955221
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-09 15:48:39 +0300

    added test_permissions_api.py

--- tests/api/permissions/test_permissions_api.py
+++ tests/api/permissions/test_permissions_api.py
@@ -4,6 +4,7 @@ from faker import Faker
 
 from apps.users.factories import UserFactory
 from apps.permissions.factories import PermissionFactory, PermissionTypeFactory
+from apps.groups.factories import GroupFactory
 from apps.permissions.models import Permission
 
 
@@ -13,25 +14,26 @@ class TestViews:
         self.client = Client()
         self.user = UserFactory()
         self.fake = Faker()
-
-        self.permission_type_read = PermissionTypeFactory(name="read")
-        self.permission_type_create = PermissionTypeFactory(name="create")
-        self.permission_type_update = PermissionTypeFactory(name="update")
-        self.permission_type_delete = PermissionTypeFactory(name="delete")
+        self.user_group = GroupFactory(group_info__owner=self.user)
+        self.other_group = GroupFactory()
         self.permission_types = {
-            "read": self.permission_type_read,
-            "create": self.permission_type_create,
-            "update": self.permission_type_update,
-            "delete": self.permission_type_delete,
+            name: PermissionTypeFactory(name=name) for name in
+            ["read", "create", "update", "delete"]
         }
+        self.client.force_login(self.user)
 
     # @pytest.mark.skip
-    def test_balances_post(self):
-        self.client.force_login(self.user)
-        PermissionFactory.create(
-            user=self.user,
-            types=(self.permission_type_read, self.permission_type_update),
-        )
+    def test_permission_get(self):
+        """Getting all user permissions"""
+        user_permission = PermissionFactory(user=self.user, group=self.user_group)
+        user_permission.types.add(self.permission_types["read"])
+        user_permission.types.add(self.permission_types["create"])
+        other_permission = PermissionFactory(user=self.user, group=self.other_group)
+        other_permission.types.add(self.permission_types["update"])
+        other_permission.types.add(self.permission_types["delete"])
 
         response = self.client.get("/api/permissions/")
-        print()
+        assert response.status_code == 200
+        assert response.data["count"] == 2
+        assert response.data["results"][0]["types"] == [self.permission_types["read"].id,
+                                                        self.permission_types["create"].id]
