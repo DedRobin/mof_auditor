f384953be4ec03ecaae3d019c1c93bf6d1256c3d
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-12 14:14:00 +0300

    updated test_permissions_api_get.py

--- tests/api/permissions/test_permissions_api_get.py
+++ tests/api/permissions/test_permissions_api_get.py
@@ -12,9 +12,12 @@ class TestViews:
     def setup_method(self):
         self.client = Client()
         self.user = UserFactory()
+        self.invited_user = UserFactory()
         self.fake = Faker()
-        self.user_group = GroupFactory(group_info__owner=self.user)
-        self.other_group = GroupFactory()
+        self.group = GroupFactory(group_info__owner=self.user)
+        self.group.invited_users.add(self.invited_user)
+
+        # Default permission types
         self.permission_types = {
             name: PermissionTypeFactory(name=name) for name in
             ["read", "create", "update", "delete"]
@@ -23,36 +26,42 @@ class TestViews:
         self.create = self.permission_types["create"]
         self.update = self.permission_types["update"]
         self.delete = self.permission_types["delete"]
+
+        # Setting permission types for user
+        self.p_for_user = PermissionFactory(user=self.user, group=self.group)
+        self.p_for_user.types.set([self.read, self.create, self.update, self.delete])
+
+        # Setting permission types for invited user
+        self.p_for_invited_user = PermissionFactory(user=self.invited_user, group=self.group)
+        self.p_for_invited_user.types.set([self.read])
+
         self.client.force_login(self.user)
 
     # @pytest.mark.skip
     def test_permission_get(self):
         """Getting all user permissions"""
 
-        p_for_user_group = PermissionFactory(user=self.user, group=self.user_group)
-        p_for_user_group.types.add(self.read)
-        p_for_user_group.types.add(self.create)
+        url = "/api/groups/{0}/invited_users/{1}/permissions/"
+        url = url.format(self.group.id, self.user.id)
 
-        p_for_other_group = PermissionFactory(user=self.user, group=self.other_group)
-        p_for_other_group.types.add(self.update)
-        p_for_other_group.types.add(self.delete)
-
-        response = self.client.get("/api/permissions/")
+        response = self.client.get(url)
         assert response.status_code == 200
-        assert response.data["count"] == 2
-        assert response.data["results"][0]["types"] == [self.read.id, self.create.id]
-        assert response.data["results"][1]["types"] == [self.update.id, self.delete.id]
+        assert response.data["results"][0]["types"] == [self.read.id, self.create.id, self.update.id, self.delete.id]
+
+        url = "/api/groups/{0}/invited_users/{1}/permissions/"
+        url = url.format(self.group.id, self.invited_user.id)
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert response.data["results"][0]["types"] == [self.read.id]
 
     # @pytest.mark.skip
     def test_permission_get_one_note(self):
-        """Getting all user permissions"""
+        """Getting a specific permission"""
 
-        permission = PermissionFactory(user=self.user, group=self.user_group)
-        permission.types.set([self.read, self.create, self.update, self.delete])
+        url = "/api/groups/{0}/invited_users/{1}/permissions/{2}/"
+        url = url.format(self.group.id, self.invited_user.id, self.p_for_invited_user.id)
 
-        response = self.client.get(f"/api/permissions/{permission.id}/")
+        response = self.client.get(url)
         assert response.status_code == 200
-        assert response.data["id"] == permission.id
-        assert response.data["user"] == self.user.username
-        assert response.data["group"] == self.user_group.__str__()
-        assert response.data["types"] == [self.read.id, self.create.id, self.update.id, self.delete.id]
+        assert response.data["id"] == self.p_for_invited_user.id
+        assert response.data["types"] == [self.read.id]
