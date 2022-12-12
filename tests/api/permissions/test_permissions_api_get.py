e3b6f3c5e7e0e71ea0184daf3ec583c4ac3c1796
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-12 10:59:01 +0300

    fixed test_permissions_api_get.py

--- tests/api/permissions/test_permissions_api_get.py
+++ tests/api/permissions/test_permissions_api_get.py
@@ -19,30 +19,40 @@ class TestViews:
             name: PermissionTypeFactory(name=name) for name in
             ["read", "create", "update", "delete"]
         }
+        self.read = self.permission_types["read"]
+        self.create = self.permission_types["create"]
+        self.update = self.permission_types["update"]
+        self.delete = self.permission_types["delete"]
         self.client.force_login(self.user)
 
     # @pytest.mark.skip
     def test_permission_get(self):
         """Getting all user permissions"""
-        user_permission = PermissionFactory(user=self.user, group=self.user_group)
-        user_permission.types.add(self.permission_types["read"])
-        user_permission.types.add(self.permission_types["create"])
-        other_permission = PermissionFactory(user=self.user, group=self.other_group)
-        other_permission.types.add(self.permission_types["update"])
-        other_permission.types.add(self.permission_types["delete"])
+
+        p_for_user_group = PermissionFactory(user=self.user, group=self.user_group)
+        p_for_user_group.types.add(self.read)
+        p_for_user_group.types.add(self.create)
+
+        p_for_other_group = PermissionFactory(user=self.user, group=self.other_group)
+        p_for_other_group.types.add(self.update)
+        p_for_other_group.types.add(self.delete)
 
         response = self.client.get("/api/permissions/")
         assert response.status_code == 200
         assert response.data["count"] == 2
-        assert response.data["results"][0]["types"] == [self.permission_types["read"].id,
-                                                        self.permission_types["create"].id]
+        assert response.data["results"][0]["types"] == [self.read.id, self.create.id]
+        assert response.data["results"][1]["types"] == [self.update.id, self.delete.id]
 
     # @pytest.mark.skip
     def test_permission_get_one_note(self):
         """Getting all user permissions"""
 
         permission = PermissionFactory(user=self.user, group=self.user_group)
+        permission.types.set([self.read, self.create, self.update, self.delete])
 
         response = self.client.get(f"/api/permissions/{permission.id}/")
         assert response.status_code == 200
         assert response.data["id"] == permission.id
+        assert response.data["user"] == self.user.username
+        assert response.data["group"] == self.user_group.__str__()
+        assert response.data["types"] == [self.read.id, self.create.id, self.update.id, self.delete.id]
