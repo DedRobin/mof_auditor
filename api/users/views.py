a35116b63eb9951b843db0e6fb5f6c2056020cb6
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-13 16:00:07 +0300

    some fixed user API

--- api/users/views.py
+++ api/users/views.py
@@ -19,36 +19,3 @@ class UserViewSet(viewsets.ModelViewSet):
             user = User.objects.filter(pk=user_id)
             return user
         return users
-
-    def get(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_200_OK)
-
-    #
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #
-    #     create_transaction_api(
-    #         balance_id=kwargs.get("pk"), validated_data=serializer.validated_data
-    #     )
-    #
-    #     return Response(status=status.HTTP_201_CREATED)
-    #
-    # def update(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     transaction_id = kwargs.get("transaction_id")
-    #
-    #     update_transaction_api(
-    #         transaction_id=transaction_id,
-    #         validated_data=serializer.validated_data,
-    #     )
-    #
-    #     return Response(status=status.HTTP_200_OK)
-    #
-    # def destroy(self, request, *args, **kwargs):
-    #     transaction_id = kwargs.get("transaction_id")
-    #
-    #     Transaction.objects.get(pk=transaction_id).delete()
-    #
-    #     return Response(status=status.HTTP_200_OK)
