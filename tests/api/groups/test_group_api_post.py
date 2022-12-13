d8971af009c9447b63fa4c7b81390ab7f26e0648
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-13 08:51:34 +0300

    fixed test_group_api_post.py

--- tests/api/groups/test_group_api_post.py
+++ tests/api/groups/test_group_api_post.py
@@ -2,8 +2,8 @@ import pytest
 from django.test.client import Client
 from faker import Faker
 
+from apps.permissions.factories import PermissionTypeFactory
 from apps.users.factories import UserFactory
-from apps.groups.factories import GroupInformationFactory, GroupFactory
 
 
 @pytest.mark.django_db
@@ -13,6 +13,16 @@ class TestViews:
         self.user = UserFactory()
         self.fake = Faker()
         self.client.force_login(self.user)
+
+        # Default permission types
+        self.permission_types = {
+            name: PermissionTypeFactory(name=name) for name in
+            ["read", "create", "update", "delete"]
+        }
+        self.read = self.permission_types["read"]
+        self.create = self.permission_types["create"]
+        self.update = self.permission_types["update"]
+        self.delete = self.permission_types["delete"]
 
     # @pytest.mark.skip
     def test_group_post(self):
