f78469cfd0755dcffeab248cf33c9c40166b6287
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-20 16:27:43 +0300

    update management command for creating users

--- apps/users/management/commands/generate_users.py
+++ apps/users/management/commands/generate_users.py
@@ -3,6 +3,8 @@ from faker import Faker
 from django.core.management.base import BaseCommand
 
 from apps.users.models import User
+from apps.users.factories import UserFactory
+from apps.profiles.factories import ProfileFactory
 from apps.profiles.models import Profile, GENDER_CHOICE
 
 fake = Faker()
@@ -10,23 +12,23 @@ gender_choice = [gender[0] for gender in GENDER_CHOICE]
 
 
 class Command(BaseCommand):
-    help = "Generate users"
+    help = "Create random users with their profiles."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--clear",
+            type=bool,
+            help="Delete all user records",
+        )
 
     def handle(self, *args, **options):
-        i = 0
-        number = int(input("How much users do you want to create?\nEnter number: "))
-        while i < number:
-            random_gender = random.choice(gender_choice)
-
-            user = User.objects.create(username=fake.user_name())
-            user.set_password("password")
-            user.save()
-            Profile.objects.create(
-                user=user,
-                email=fake.email(),
-                gender=random_gender,
-                first_name=fake.first_name(),
-                last_name=fake.last_name(),
-            )
-            i += 1
-        print(f"Create {number} users.")
+        if options.get("clear", None):
+            User.objects.all().exclude(is_superuser=True).delete()
+        # try:
+        size = int(input("How much would you like create users?\n"))
+        # except:
+        # print(f'The value "{size}" is incorrect. Enter a number greater than 0.')
+        for _ in range(size):
+            user = UserFactory()
+            ProfileFactory(user=user)
+        print(f"The {size} users has been created.")
