1ac0a26240a3d7e00a054a68798934e7723f4572
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-20 13:42:55 +0300

    make migrations for 'reports' app

--- apps/reports/admin.py
+++ apps/reports/admin.py
@@ -6,23 +6,20 @@ from apps.reports.models import Report
 class ReportAdmin(admin.ModelAdmin):
     list_display = (
         "balance",
-        "date_from",
-        "date_to",
         "total",
+        "difference",
         "created_at",
     )
     fields = (
         "balance",
-        "date_from",
-        "date_to",
         "total",
+        "difference",
     )
     readonly_fields = ("created_at",)
 
     search_fields = (
         "balance__name",
-        "date_from",
-        "date_to",
         "total",
+        "difference",
         "created_at",
     )
--- apps/reports/models.py
+++ apps/reports/models.py
@@ -6,7 +6,6 @@ class Report(models.Model):
     balance = models.ForeignKey(
         Balance, on_delete=models.CASCADE, related_name="reports"
     )
-    date_from = models.DateTimeField()
-    date_to = models.DateTimeField()
-    total = models.DecimalField(max_digits=19, decimal_places=7, default=0)
+    total = models.DecimalField(max_digits=19, decimal_places=7)
+    difference = models.DecimalField(max_digits=19, decimal_places=7, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
