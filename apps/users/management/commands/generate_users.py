58db2b05c83c2299e318f3275dc6eb1ae2dc6b80
Pavel Rabekin <pavelrabekin@gmail.com>
2022-12-20 18:29:13 +0300

    updated generate_users.py

--- apps/users/management/commands/generate_users.py
+++ apps/users/management/commands/generate_users.py
@@ -1,14 +1,8 @@
-import random
-from faker import Faker
 from django.core.management.base import BaseCommand
 
 from apps.users.models import User
 from apps.users.factories import UserFactory
 from apps.profiles.factories import ProfileFactory
-from apps.profiles.models import Profile, GENDER_CHOICE
-
-fake = Faker()
-gender_choice = [gender[0] for gender in GENDER_CHOICE]
 
 
 class Command(BaseCommand):
@@ -17,18 +11,22 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "--clear",
-            type=bool,
+            action="store_true",
             help="Delete all user records",
         )
 
     def handle(self, *args, **options):
-        if options.get("clear", None):
+        if options["clear"]:
             User.objects.all().exclude(is_superuser=True).delete()
-        # try:
-        size = int(input("How much would you like create users?\n"))
-        # except:
-        # print(f'The value "{size}" is incorrect. Enter a number greater than 0.')
-        for _ in range(size):
-            user = UserFactory()
-            ProfileFactory(user=user)
-        print(f"The {size} users has been created.")
+        try:
+            size = int(input("How much would you like create users?\n"))
+            if size <= 0:
+                print("Size must be greater than 0.")
+                return
+        except ValueError:
+            print("The value is incorrect.")
+        else:
+            for _ in range(size):
+                user = UserFactory()
+                ProfileFactory(user=user)
+            print(f"The {size} users has been created.")
