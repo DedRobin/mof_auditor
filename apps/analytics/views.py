b0043066661ca91c6fd925426be12badc97d19a4
Pavel Rabekin <pavelrabekin@gmail.com>
2023-02-20 16:14:56 +0300

    added google chart (test)

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from apps.balances.models import Balance
+
 
 @login_required
 def get_charts(request):
-    return render(request, "analytics/charts.html")
+    balance = Balance.objects.filter(owner=request.user).first()
+    transactions = balance.transactions.all()
+    data = {
+        "transactions": transactions,
+        "balance": balance,
+    }
+    return render(request, "analytics/charts.html", data)
